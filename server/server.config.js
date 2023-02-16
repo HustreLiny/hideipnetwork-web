edf576b8218ff897030d602437e28c482c86efdb
HustreLiny <42056233+HustreLiny@users.noreply.github.com>
2023-02-16 16:37:16 +0800

    Update server.config.js

--- server/server.config.js
+++ server/server.config.js
@@ -1,7 +1,7 @@
 /**
  * Just replace BAREUR
  */ 
-const BAREURL = 'https://yourdomain'
+const BAREURL = 'https://test.reddisk.eu.org'
 
 /**
  * Non-developers, please modify without authorization, 98% report errors!
